177eaa4989803b81a7d9cef3b2a4f5b12d586aea
Clement Adeyemi <adeyemiclement1@gmail.com>
2022-06-16 13:13:40 +0200

    Made responsive

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-// function toCelsius(fahrenheit){
-//  return (5/9) * (fahrenheit-32);
-// }
-// document.getElementById("demo").innerHTML = toCelsius(77);
-  // 
-
 // SHOW NAV LINKS AFTER CLICKING
 function showLinks() {
   document.getElementById ("openIcon").style.display = "none";
+  document.getElementById("closeIcon").style.display = "block";
   document.getElementById("navLinks").style.display = "block";
   document.getElementById ("navLinks").style.marginTop = "0";
-  document.getElementById("closeIcon").style.display = "block";
 }
 // HIDE NAV LINKS AFTER CLICKING
 function hideLinks() {
@@ -30,8 +24,4 @@ function hideSubLink() {
 }
 function closeNav() {
   document.getElementById("transport").style.display = "none";
-}
-
-function displayOptions(){
-  document.getElementById('dropdown-trip').style.display = 'block';
 }
